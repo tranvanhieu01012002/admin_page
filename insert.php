6c21510ee3c5fcc9a63299569721b3b2a63632f7
hamongkhang <khang.ha22@student.passerellesnumeriques.org>
2022-03-15 17:30:44 +0700

    add new line

--- insert.php
+++ insert.php
@@ -4,6 +4,9 @@ $mysql = new mysqli($host, $user, $password, $database);
 if ($mysql === false) {
     die("ERROR: Could not connect" . $mysql->connect_error);
 }
+
+
+echo "aaaaaaaaaaaaaaaaaaaaaaaaaaa";
 $first_name = $mysql->real_escape_string($_POST["firstname"]);
 $last_name = $mysql->real_escape_string($_POST["lastname"]);
 $email = $mysql->real_escape_string($_POST["email"]);
