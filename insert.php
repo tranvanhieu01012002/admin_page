898f7b85adcfdeaf1e305bd83086b63aac91c47f
hamongkhang <khang.ha22@student.passerellesnumeriques.org>
2022-03-15 16:42:45 +0700

    first commit

---
+++ insert.php
@@ -0,0 +1,16 @@
+<?php
+include "../account.php";
+$mysql = new mysqli($host, $user, $password, $database);
+if ($mysql === false) {
+    die("ERROR: Could not connect" . $mysql->connect_error);
+}
+$first_name = $mysql->real_escape_string($_POST["firstname"]);
+$last_name = $mysql->real_escape_string($_POST["lastname"]);
+$email = $mysql->real_escape_string($_POST["email"]);
+$sql = "INSERT INTO persons (first_name,last_name,email) VALUES('$first_name','$last_name','$email')";
+if ($mysql->query($sql) === true) {
+    echo "Insert data successfully.";
+} else {
+    echo "ERROR: Could not able to execute $sql. " . $mysql->error;
+}
+$mysql->close();
